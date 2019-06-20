6ef1f04439d8031dbea875294907d8bf3786a0a0
vladimir <vladimir.zhuravlev@yalantis.com>
2019-06-20 11:38:17 +0300

    Add Bridge pattern realization

--- patterns/builder/Builder.ts
+++ patterns/builder/Builder.ts
@@ -1,3 +1,5 @@
+/** Separate the construction of a complex object from its representation
+* so that the same construction process can create different representations.*/
 interface IBuilder {
     producePartA(): void;
     producePartB(): void;
--- patterns/factory method/FactoryMethod.ts
+++ patterns/factory method/FactoryMethod.ts
@@ -1,3 +1,7 @@
+/** Define an interface for creating an object,
+* but let subclasses decide which class to instantiate.
+* Factory Method lets a class defer instantiation to subclasses.*/
+
 interface Product {
   operation(): string;
 }
